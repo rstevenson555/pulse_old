dc14b516288f0b467d2922181c28f7df6acd60b6
i0360b6 <>
2013-04-30 19:41:37 +0000

    *** empty log message ***

---
+++ java/logParser/classbuilder/ClassListCollection.java
@@ -0,0 +1,21 @@
+/*
+ * ClassCollection.java
+ *
+ * Created on May 15, 2002, 7:56 PM
+ */
+package logParser.classbuilder;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author  Will Houck
+ * @version
+ */
+public class ClassListCollection extends LinkedList
+{
+
+    /** Creates new ClassCollection */
+    public ClassListCollection() {}
+
+}
