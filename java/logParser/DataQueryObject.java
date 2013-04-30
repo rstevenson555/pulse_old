22a1b17136160b59ce5573964c766e12e02ae350
i0360b6 <>
2013-04-30 19:22:17 +0000

    *** empty log message ***

---
+++ java/logParser/DataQueryObject.java
@@ -0,0 +1,21 @@
+/*
+ * DataQueryObject.java
+ *
+ * Created on March 30, 2001, 8:36 AM
+ */
+
+package logParser;
+import graph.*;
+
+/**
+ *
+ * @author  i0360d3
+ * @version 
+ */
+public interface DataQueryObject {
+    DataSet getDataSet();
+    void writeCSVData();
+    
+    
+}
+
