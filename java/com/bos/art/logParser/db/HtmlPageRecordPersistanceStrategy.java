3db8a59fe3cf0c6e622f5e019ea9c82511ad6060
Bob Stevenson <bobstevenson@officemax.com>
2013-05-16 12:02:51 -0500

    increase the batch size

--- java/com/bos/art/logParser/db/HtmlPageRecordPersistanceStrategy.java
+++ java/com/bos/art/logParser/db/HtmlPageRecordPersistanceStrategy.java
@@ -21,7 +21,7 @@ import org.apache.log4j.Logger;
  */
 public class HtmlPageRecordPersistanceStrategy extends BasePersistanceStrategy implements PersistanceStrategy {
 
-    private final static int MAXBATCHINSERTSIZE = 200;
+    private final static int MAXBATCHINSERTSIZE = 5000;
     private final static int INCREMENT_AMOUNT = 10;
     private final static int MINBATCHINSERTSIZE = 10;
     private static int currentBatchInsertSize = MINBATCHINSERTSIZE;
