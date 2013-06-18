d9c651a9f56670b3b12f5fe1b53ac9e0c01a3a4b
Bob Stevenson <bobstevenson@officemax.com>
2013-06-18 17:02:39 -0500

    increase batch sizes

--- java/com/bos/art/logParser/collector/QueryParameterWriteQueue.java
+++ java/com/bos/art/logParser/collector/QueryParameterWriteQueue.java
@@ -34,9 +34,9 @@ public class QueryParameterWriteQueue extends Thread implements Serializable {
     private int objectsWritten;
     private long totalWriteTime;
 
-    private final static int MAXBATCHINSERTSIZE = 2000;
+    private final static int MAXBATCHINSERTSIZE = 3000;
     private final static int INCREMENT_AMOUNT = 10;
-    private final static int MINBATCHINSERTSIZE = 900;
+    private final static int MINBATCHINSERTSIZE = 2000;
     private static int currentBatchInsertSize = MINBATCHINSERTSIZE;
     private static double timePerInsert = 5000.0;
 
