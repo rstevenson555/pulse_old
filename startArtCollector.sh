8170991cce8f30be0602fc844d885f3998276fa6
Bob Stevenson <bobstevenson@officemax.com>
2013-06-20 08:46:14 -0500

    encoded the input stream

--- startArtCollector.sh
+++ startArtCollector.sh
@@ -2,4 +2,4 @@
 export JAVA_HOME=/opt/art/java/jdk1.6.0_29
 export PATH=$JAVA_HOME/bin:$CATALINA_HOME/bin:$PATH
 
-nohup ant start-collector-sol -Dparam=-server -logfile ./ArtCollectorStdout.log &
+nohup ant start-collector-sol -Dparam=-server -Dparam2=-encode_input -logfile ./ArtCollectorStdout.log &
