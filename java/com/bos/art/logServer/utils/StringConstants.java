4113c61bf4f9edcfd258555362dd9ac161672eed
Bob Stevenson <bobstevenson@officemax.com>
2013-08-01 10:41:03 -0500

    new StringConstants class

---
+++ java/com/bos/art/logServer/utils/StringConstants.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bos.art.logServer.utils;
+
+/**
+ *
+ * @author i0360b6
+ */
+public interface StringConstants {
+    static final String CONTEXT = "#CONTEXT#";
+    static final String MACHINE_TYPE = "#MACHINE_TYPE#";
+    static final String INSTANCE_TYPE = "#INSTANCE_TYPE#";
+    static final String PAGE = "#PAGE#";
+
+}
