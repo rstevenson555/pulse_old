756e9081dd8b857fe35ef698b3f89de10882016b
Bob Stevenson <bobstevenson@officemax.com>
2013-11-28 22:31:40 -0600

    decrease queue sizes a little

--- java/com/bos/art/logServer/utils/PatchFilterInputStream.java
+++ java/com/bos/art/logServer/utils/PatchFilterInputStream.java
@@ -19,23 +19,27 @@ public class PatchFilterInputStream extends FilterInputStream {
 
         int b = in.read();
 
-        switch(b) {
+        switch (b) {
             case (byte) '\u0000':
-                return (byte)' ';
+                return (byte) ' ';
             case (byte) '\u0002':
-                return (byte)' ';
+                return (byte) ' ';
             case (byte) '\u001f':
-                return (byte)' ';
+                return (byte) ' ';
             case (byte) '\u001e':
-                return (byte)' ';
+                return (byte) ' ';
             case (byte) '\u0018':
-                return (byte)' ';
+                return (byte) ' ';
             case (byte) '\u0003':
-                return (byte)' ';
+                return (byte) ' ';
             case (byte) '\u001d':
-                return (byte)' ';
+                return (byte) ' ';
             case (byte) '\u0012':
-                return (byte)' ';
+                return (byte) ' ';
+            case (byte) '\u000f':
+                return (byte) ' ';
+            case (byte) '\u0013':
+                return (byte) ' ';
         }
 
 
@@ -49,23 +53,27 @@ public class PatchFilterInputStream extends FilterInputStream {
         for (int i = offset; i < offset + result; i++) {
             // do nothing with the printing characters
             // carriage return, linefeed, tab, and end of file
-            switch(data[i]) {
+            switch (data[i]) {
                 case (byte) '\u0000':
-                    data[i] = (byte)' ';
+                    data[i] = (byte) ' ';
                 case (byte) '\u0002':
-                    data[i] = (byte)' ';
+                    data[i] = (byte) ' ';
                 case (byte) '\u001f':
-                    data[i] = (byte)' ';
+                    data[i] = (byte) ' ';
                 case (byte) '\u001e':
-                    data[i] = (byte)' ';
+                    data[i] = (byte) ' ';
                 case (byte) '\u0018':
-                    data[i] = (byte)' ';
+                    data[i] = (byte) ' ';
                 case (byte) '\u0003':
-                    data[i] = (byte)' ';
+                    data[i] = (byte) ' ';
                 case (byte) '\u001d':
-                    data[i] = (byte)' ';
+                    data[i] = (byte) ' ';
                 case (byte) '\u0012':
-                    data[i] = (byte)' ';
+                    data[i] = (byte) ' ';
+                case (byte) '\u000f':
+                    data[i] = (byte) ' ';
+                case (byte) '\u0013':
+                    data[i] = (byte) ' ';
             }
         }
         return result;
