78662a116e9b7dca8811fa75aff1ef45cd44e0eb
Bob Stevenson <bobstevenson@officemax.com>
2013-12-09 15:11:44 -0600

    use LMAX distruptor

--- java/com/bos/art/logParser/collector/DatabaseWriteQueue.java
+++ java/com/bos/art/logParser/collector/DatabaseWriteQueue.java
@@ -9,9 +9,12 @@ package com.bos.art.logParser.collector;
 
 import com.bos.art.logParser.records.ILiveLogParserRecord;
 import java.io.Serializable;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.*;
+
+import com.lmax.disruptor.*;
+import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.dsl.ProducerType;
+import com.lmax.disruptor.util.DaemonThreadFactory;
 import org.apache.log4j.Logger;
 
 /**
@@ -28,13 +31,73 @@ public class DatabaseWriteQueue extends Thread implements Serializable {
     private int objectsWritten;                 
     private long totalWriteTime;
     protected static boolean unloadDB = true;
-    private static final int MAX_DB_QUEUE_SIZE = 3500;
+    private static final int MAX_DB_QUEUE_SIZE = 4500;
     private static long fullCount = 0;
     private static long writeCount = 0;
+    private final ExecutorService executor = Executors.newSingleThreadExecutor(DaemonThreadFactory.INSTANCE);
+
+    private Disruptor<ILiveLogParserRecordEvent> disruptor = new Disruptor<ILiveLogParserRecordEvent>(ILiveLogParserRecordEvent.FACTORY, 5000, executor,
+                ProducerType.SINGLE, new BusySpinWaitStrategy());
+
     // guards for boundaries
+
+    private static class ILiveLogParserRecordEvent
+    {
+        private ILiveLogParserRecord record;
+
+        public static final EventFactory<ILiveLogParserRecordEvent> FACTORY = new EventFactory<DatabaseWriteQueue.ILiveLogParserRecordEvent>()
+        {
+            public ILiveLogParserRecordEvent newInstance()
+            {
+                return new ILiveLogParserRecordEvent();
+            }
+        };
+    };
+
+    private class LiveLogParserRecordEventHandler implements EventHandler<ILiveLogParserRecordEvent>
+    {
+        public int failureCount = 0;
+        public int messagesSeen = 0;
+
+        public LiveLogParserRecordEventHandler()
+        {
+        }
+
+        public void onEvent(ILiveLogParserRecordEvent event, long sequence, boolean endOfBatch) throws Exception
+        {
+//            if (event.sequence != sequence ||
+//                    event.a != sequence + 13 ||
+//                    event.b != sequence - 7 ||
+//                    !("wibble-" + sequence).equals(event.s))
+//            {
+//                failureCount++;
+//            }
+
+            ILiveLogParserRecord ilpr = event.record;
+            ++objectsRemoved;
+
+            long writeStartTime = System.currentTimeMillis();
+
+            if (writeCount++ % 1000 == 0) {
+                //logger.warn("DatabaseWriteQueue writeToDatabase called");
+            }
+            logger.warn("Persisting...");
+            ilpr.writeToDatabase();
+            totalWriteTime += (System.currentTimeMillis() - writeStartTime);
+            ++objectsWritten;
+            messagesSeen++;
+        }
+    }
+
 
     private DatabaseWriteQueue() {
         dequeue = new ArrayBlockingQueue<ILiveLogParserRecord>(MAX_DB_QUEUE_SIZE);
+
+        RingBuffer<ILiveLogParserRecordEvent> ringBuffer = disruptor.getRingBuffer();
+        disruptor.handleExceptionsWith(new FatalExceptionHandler());
+        LiveLogParserRecordEventHandler handler = new LiveLogParserRecordEventHandler();
+        disruptor.handleEventsWith(handler);
+        disruptor.start();
     }
 
     public static DatabaseWriteQueue getInstance() {
@@ -46,11 +109,18 @@ public class DatabaseWriteQueue extends Thread implements Serializable {
         addLast((ILiveLogParserRecord)o);
     }
 
-    public void addLast(ILiveLogParserRecord o) {
-        boolean success = dequeue.offer(o);
-        if (!success && (fullCount++ % 100) == 0) {
-            logger.error("DatabaseWriteQueue is full, throwing out messages");
-        }
+    public void addLast(final ILiveLogParserRecord o) {
+        //boolean success = dequeue.offer(o);
+        disruptor.publishEvent(new EventTranslator<ILiveLogParserRecordEvent>() {
+            public void translateTo(ILiveLogParserRecordEvent event, long sequence)
+            {
+                event.record = o;
+            }
+
+        });
+//        if (!success && (fullCount++ % 100) == 0) {
+//            logger.error("DatabaseWriteQueue is full, throwing out messages");
+//        }
     }
 
 
