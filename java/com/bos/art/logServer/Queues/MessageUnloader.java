6cd637f105d0bee8cfde3d8dc904855953d99241
Bob Stevenson <bobstevenson@officemax.com>
2014-01-10 14:35:06 -0600

    improve socket buffering

--- java/com/bos/art/logServer/Queues/MessageUnloader.java
+++ java/com/bos/art/logServer/Queues/MessageUnloader.java
@@ -8,7 +8,9 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -294,13 +296,16 @@ public class MessageUnloader extends java.lang.Thread {
 
         try {
             logger.info("trying to connect to ArtEngine at: " + address + " port: " + port);
-            Socket socket = new Socket(address, port);
+            Socket socket = new Socket();
 
             logger.warn("Socket Buffer Size: " + socket.getReceiveBufferSize());
             socket.setSendBufferSize(262144);
             logger.warn("Socket Buffer Size after change: " + socket.getReceiveBufferSize());
 
-            outputStream = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            SocketAddress engineServerAddress = new InetSocketAddress(address,port);
+            socket.bind(engineServerAddress);
+
+            outputStream = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream(),16*1024));
             logger.info("Success connecting to ArtEngine at:" + address + " port: " + port);
         } catch (IOException io) {
             logger.error("Error connecting to : " + address + " " + io);
