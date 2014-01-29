58f14d2290e5ef19dc74ba45b08d8e735169597a
Bob Stevenson <bobstevenson@officemax.com>
2014-01-29 15:57:54 -0600

    new mbean definition

---
+++ java/com/bos/art/logParser/collector/DatabaseWriteQueueMBean.java
@@ -0,0 +1,13 @@
+package com.bos.art.logParser.collector;
+
+import com.bos.art.logServer.Queues.MessageUnloaderMBean;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: i0360b6
+ * Date: 1/29/14
+ * Time: 3:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DatabaseWriteQueueMBean extends MessageUnloaderMBean {
+}
