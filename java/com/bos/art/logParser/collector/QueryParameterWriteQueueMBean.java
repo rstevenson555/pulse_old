d306bd743ee353c5f8be0236feeb63be774f8e07
Bob Stevenson <bobstevenson@officemax.com>
2014-01-30 08:40:02 -0600

    new mbean

---
+++ java/com/bos/art/logParser/collector/QueryParameterWriteQueueMBean.java
@@ -0,0 +1,11 @@
+package com.bos.art.logParser.collector;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: i0360b6
+ * Date: 1/30/14
+ * Time: 8:37 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface QueryParameterWriteQueueMBean extends QueryParameterProcessingQueueMBean {
+}
