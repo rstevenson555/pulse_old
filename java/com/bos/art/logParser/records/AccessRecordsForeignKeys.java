43498422c5df9c736dcf51609ade14b27aa40241
Bob Stevenson <bobstevenson@officemax.com>
2014-02-17 09:46:55 -0600

    fix formatting

--- java/com/bos/art/logParser/records/AccessRecordsForeignKeys.java
+++ java/com/bos/art/logParser/records/AccessRecordsForeignKeys.java
@@ -13,59 +13,46 @@
 package com.bos.art.logParser.records;
 
 
-
 import java.io.Serializable;
 
 
-
 /**
-
  * @author I0360D3
-
- *
-
- * To change the template for this generated type comment go to
-
- * Window>Preferences>Java>Code Generation>Code and Comments
-
+ *         <p/>
+ *         <p/>
+ *         <p/>
+ *         To change the template for this generated type comment go to
+ *         <p/>
+ *         Window>Preferences>Java>Code Generation>Code and Comments
  */
 
 public class AccessRecordsForeignKeys implements Serializable {
 
-	public AccessRecordsForeignKeys(java.util.Date date){
+    public int fkContextID = 0;
+    public int fkUserID = 0;
+    public int fkSessionID = 0;
+    public int fkMachineID = 0;
+    public int fkInstanceID = 0;
+    public int fkPageID = 0;
+    public int fkAppID = 0;
+    public int fkBranchTagID = 0;
+    public int fkQueryParameterID = 0;
+    public java.util.Date eventTime;
 
-		eventTime=date;
+    public AccessRecordsForeignKeys(java.util.Date date) {
 
-	}
+        eventTime = date;
 
-	/** Don't create one of these with this method... */
+    }
 
-	private AccessRecordsForeignKeys(){
+    /**
+     * Don't create one of these with this method...
+     */
 
-		
+    private AccessRecordsForeignKeys() {
 
-	}
 
-	public int fkContextID =0;
-
-	public int fkUserID= 0;
-
-	public int fkSessionID = 0;
-
-	public int fkMachineID = 0;
-    
-	public int fkInstanceID = 0;
-
-	public int fkPageID = 0;
-
-	public int fkAppID = 0;
-
-	public int fkBranchTagID = 0;
-
-	public int fkQueryParameterID = 0;
-
-	public java.util.Date eventTime;
-
+    }
 
 
 }
