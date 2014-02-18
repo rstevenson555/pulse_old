c482a35b4742529cbe2f03baa99d2bf95f1dbc7d
Bob Stevenson <bobstevenson@officemax.com>
2014-02-18 09:18:16 -0600

    fix formatting of mbean name

--- java/com/bos/art/logParser/collector/QueryParameterProcessingQueue.java
+++ java/com/bos/art/logParser/collector/QueryParameterProcessingQueue.java
@@ -115,7 +115,7 @@ public class QueryParameterProcessingQueue implements QueryParameterProcessingQu
         MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
         ObjectName name = null;
         try {
-            name = new ObjectName("com.omx.engine:type=QueryParameterProcessingQueueMBean"+(++qpInstance));
+            name = new ObjectName("com.omx.engine:type=QueryParameterProcessingQueueMBean-"+(++qpInstance));
             mbs.registerMBean(this, name);
         } catch (MalformedObjectNameException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
