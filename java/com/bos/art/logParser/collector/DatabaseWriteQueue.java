6abde0f990f98e6d42521e5208e2a1cb60cea886
Bob Stevenson <bobstevenson@officemax.com>
2014-02-21 08:51:03 -0600

    decrease queue size

--- java/com/bos/art/logParser/collector/DatabaseWriteQueue.java
+++ java/com/bos/art/logParser/collector/DatabaseWriteQueue.java
@@ -45,7 +45,7 @@ public class DatabaseWriteQueue implements DatabaseWriteQueueMBean,Serializable 
     private final ExecutorService executor = Executors.newSingleThreadExecutor(tFactory);
     private TPSCalculator tpsCalculator = new TPSCalculator();
 
-    private Disruptor<ILiveLogParserRecordEvent> disruptor = new Disruptor<ILiveLogParserRecordEvent>(ILiveLogParserRecordEvent.FACTORY, 128, executor,
+    private Disruptor<ILiveLogParserRecordEvent> disruptor = new Disruptor<ILiveLogParserRecordEvent>(ILiveLogParserRecordEvent.FACTORY, 64, executor,
                 ProducerType.SINGLE, new BlockingWaitStrategy());
 
     // guards for boundaries
